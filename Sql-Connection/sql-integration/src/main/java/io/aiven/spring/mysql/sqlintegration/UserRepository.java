0d4459a2265020ad4b1f16db40be30b4d088fdb3
Jayant Bokade <112440030+mrjayantbokade@users.noreply.github.com>
2025-01-26 18:17:29 +0530

    Delete Sql-Connection/sql-integration directory

--- Sql-Connection/sql-integration/src/main/java/io/aiven/spring/mysql/sqlintegration/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package io.aiven.spring.mysql.sqlintegration;
-
-import org.springframework.data.repository.CrudRepository;
-import io.aiven.spring.mysql.sqlintegration.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-    User findByEmail(String email);
-}
